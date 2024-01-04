cc3d5ea903eb57d0f5ea7269e92543caec390545
sabareesh-ed <sabareeshed@gmail.com>
2024-01-04 16:07:23 +0530

    test

--- script.js
+++ script.js
@@ -91,50 +91,121 @@ document.addEventListener("DOMContentLoaded", function () {
     button.addEventListener("click", handleButtonClick);
   });
 
+  // function renderCartItems() {
+  //   let storedItems = JSON.parse(localStorage.getItem("quotedItems")) || [];
+  //   let cartContentWrapper = document.getElementById(
+  //     "navbar_cart-content-wrapper"
+  //   );
+
+  //   cartContentWrapper.innerHTML = "";
+
+  //   storedItems.forEach((item, index) => {
+  //     let itemDiv = document.createElement("div");
+  //     itemDiv.className = "navbar_cart-content";
+
+  //     let itemNameSpan = document.createElement("span");
+  //     itemNameSpan.className = "text-size-small font-dm-sans text-weight-bold"
+  //     itemNameSpan.textContent = item.name;
+
+  //     let deleteButton = document.createElement("button");
+  //     deleteButton.className = "navbar-cart-deletebtn";
+
+  //     let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  //     svg.setAttribute("viewBox", "0 0 24 24");
+  //     svg.setAttribute("fill", "none");
+  //     svg.setAttribute("stroke", "currentColor");
+  //     svg.setAttribute("stroke-width", "1.5");
+  //     svg.innerHTML = `<path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />`;
+
+  //     deleteButton.appendChild(svg);
+
+  //     // Attach the click event listener to the delete button
+  //     deleteButton.addEventListener("click", function () {
+  //       removeItemFromCart(index);
+  //     });
+
+  //     itemDiv.appendChild(itemNameSpan);
+  //     itemDiv.appendChild(deleteButton);
+
+  //     cartContentWrapper.appendChild(itemDiv);
+  //   });
+  // }
+
   function renderCartItems() {
     let storedItems = JSON.parse(localStorage.getItem("quotedItems")) || [];
-    let cartContentWrapper = document.getElementById(
-      "navbar_cart-content-wrapper"
-    );
+    let cartContentWrapper = document.getElementById("navbar_cart-content-wrapper");
+    cartContentWrapper.innerHTML = '';
 
-    cartContentWrapper.innerHTML = "";
+    let groupedItems = groupItemsByType(storedItems);
 
-    storedItems.forEach((item, index) => {
-      let itemDiv = document.createElement("div");
-      itemDiv.className = "navbar_cart-content";
+    for (let type in groupedItems) {
+        let typeDiv = document.createElement("div");
+        typeDiv.className = "cart-type";
 
-      let itemNameSpan = document.createElement("span");
-      itemNameSpan.className = "text-size-small font-dm-sans text-weight-bold"
-      itemNameSpan.textContent = item.name;
+        let typeTitle = document.createElement("h3");
+        typeTitle.textContent = type;
+        typeDiv.appendChild(typeTitle);
 
-      let deleteButton = document.createElement("button");
-      deleteButton.className = "navbar-cart-deletebtn";
+        groupedItems[type].forEach((item, index) => {
+            let itemDiv = document.createElement("div");
+            itemDiv.className = "navbar_cart-content";
 
-      let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-      svg.setAttribute("viewBox", "0 0 24 24");
-      svg.setAttribute("fill", "none");
-      svg.setAttribute("stroke", "currentColor");
-      svg.setAttribute("stroke-width", "1.5");
-      svg.innerHTML = `<path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />`;
+            let itemNameSpan = document.createElement("span");
+            itemNameSpan.className = "text-size-small font-dm-sans text-weight-bold";
+            itemNameSpan.textContent = item.name;
 
-      deleteButton.appendChild(svg);
+            let deleteButton = createDeleteButton(index, item.id);
 
-      // Attach the click event listener to the delete button
-      deleteButton.addEventListener("click", function () {
-        removeItemFromCart(index);
-      });
+            itemDiv.appendChild(itemNameSpan);
+            itemDiv.appendChild(deleteButton);
 
-      itemDiv.appendChild(itemNameSpan);
-      itemDiv.appendChild(deleteButton);
+            typeDiv.appendChild(itemDiv);
+        });
 
-      cartContentWrapper.appendChild(itemDiv);
-    });
+        cartContentWrapper.appendChild(typeDiv);
+    }
   }
 
-  function removeItemFromCart(index) {
+  function createDeleteButton(index, itemId) {
+      let deleteButton = document.createElement("button");
+      deleteButton.className = "navbar-cart-deletebtn";
+      
+      let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        svg.setAttribute("viewBox", "0 0 24 24");
+        svg.setAttribute("fill", "none");
+        svg.setAttribute("stroke", "currentColor");
+        svg.setAttribute("stroke-width", "1.5");
+        svg.innerHTML = `<path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />`;
+
+        deleteButton.appendChild(svg);
+
+      deleteButton.addEventListener("click", function() {
+          removeItemFromCart(itemId);
+      });
+
+      return deleteButton;
+  }
+
+
+  function groupItemsByType(items) {
+    const groupedItems = {};
+
+    items.forEach(item => {
+        if (!groupedItems[item.type]) {
+            groupedItems[item.type] = [];
+        }
+        if (!groupedItems[item.type].some(existingItem => existingItem.id === item.id)) {
+            groupedItems[item.type].push(item);
+        }
+    });
+
+    return groupedItems;
+  }
+
+  function removeItemFromCart(itemId) {
     let storedItems = JSON.parse(localStorage.getItem("quotedItems")) || [];
-    storedItems.splice(index, 1); // Remove the item at the specified index
-    localStorage.setItem("quotedItems", JSON.stringify(storedItems));
-    renderCartItems(); // Re-render the cart items
+    let updatedItems = storedItems.filter(item => item.id !== itemId);
+    localStorage.setItem("quotedItems", JSON.stringify(updatedItems));
+    renderCartItems();
   }
 });
