51679ca8c20e80ab5eb271ae1da82cef657eb2a5
sabareesh-ed <sabareeshed@gmail.com>
2024-01-04 12:31:48 +0530

    test

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("script loaded!");
+  console.log("test");
   renderCartItems();
   $(document).ready(function () {
     $(".product-card").on("click", function () {
