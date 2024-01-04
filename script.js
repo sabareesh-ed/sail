32e9cadd4d1324bf6762fd3fd8067e8679ac91d3
sabareesh-ed <sabareeshed@gmail.com>
2024-01-04 14:31:07 +0530

    test

--- script.js
+++ script.js
@@ -127,8 +127,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
       cartContentWrapper.appendChild(itemDiv);
     });
-
-    toggleCartDisplay()
   }
 
   function removeItemFromCart(index) {
