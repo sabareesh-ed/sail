3ff042c7cb58ad85b40b9b46943623bc384bf5c5
sabareesh-ed <sabareeshed@gmail.com>
2024-03-07 11:29:31 +0530

    asda

--- pleo.js
+++ pleo.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Define the SVG markup
     const svgMarkup = `
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" height="24px" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
         </svg>
     `;
